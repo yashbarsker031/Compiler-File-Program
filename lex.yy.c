318a688f5af3e2145c554306d0b13c43b437a9fe
yashbarsker031 <79683529+yashbarsker031@users.noreply.github.com>
2022-11-14 22:57:59 +0530

    Program3

--- lex.yy.c
+++ lex.yy.c
@@ -282,29 +282,29 @@ static void yy_fatal_error YY_PROTO(( yyconst char msg[] ));
 	*yy_cp = '\0'; \
 	yy_c_buf_p = yy_cp;
 
-#define YY_NUM_RULES 3
-#define YY_END_OF_BUFFER 4
-static yyconst short int yy_accept[8] =
+#define YY_NUM_RULES 4
+#define YY_END_OF_BUFFER 5
+static yyconst short int yy_accept[9] =
     {   0,
-        0,    0,    4,    3,    1,    2,    0
+        0,    0,    5,    2,    3,    1,    2,    0
     } ;
 
 static yyconst int yy_ec[256] =
     {   0,
+        1,    1,    1,    1,    1,    1,    1,    1,    2,    3,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    2,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    2,    3,    3,    3,    2,    3,
-        3,    3,    2,    3,    3,    3,    3,    3,    2,    3,
-        3,    3,    3,    3,    2,    3,    3,    3,    3,    3,
-        1,    1,    1,    1,    1,    1,    2,    3,    3,    3,
 
-        2,    3,    3,    3,    2,    3,    3,    3,    3,    3,
-        2,    3,    3,    3,    3,    3,    2,    3,    3,    3,
-        3,    3,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -323,27 +323,27 @@ static yyconst int yy_ec[256] =
 
 static yyconst int yy_meta[4] =
     {   0,
-        1,    1,    1
+        1,    2,    2
     } ;
 
-static yyconst short int yy_base[8] =
+static yyconst short int yy_base[10] =
     {   0,
-        0,    0,    4,    5,    5,    5,    5
+        0,    0,    5,    0,    6,    6,    0,    6,    3
     } ;
 
-static yyconst short int yy_def[8] =
+static yyconst short int yy_def[10] =
     {   0,
-        7,    1,    7,    7,    7,    7,    0
+        8,    1,    8,    9,    8,    8,    9,    0,    8
     } ;
 
-static yyconst short int yy_nxt[9] =
+static yyconst short int yy_nxt[10] =
     {   0,
-        4,    5,    6,    7,    3,    7,    7,    7
+        4,    5,    6,    7,    8,    3,    8,    8,    8
     } ;
 
-static yyconst short int yy_chk[9] =
+static yyconst short int yy_chk[10] =
     {   0,
-        1,    1,    1,    3,    7,    7,    7,    7
+        1,    1,    1,    9,    3,    8,    8,    8,    8
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -357,12 +357,11 @@ static char *yy_last_accepting_cpos;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "program2.l"
+#line 1 "program3.l"
 #define INITIAL 0
-#line 2 "program2.l"
-	int vow_count=0;
-	int const_count =0;
-#line 366 "lex.yy.c"
+#line 2 "program3.l"
+int nlines,nwords,nchars;
+#line 365 "lex.yy.c"
 
 /* Macros after this point can all be overridden by user definitions in
  * section 1.
@@ -513,9 +512,9 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
 
-#line 6 "program2.l"
+#line 5 "program3.l"
 
-#line 519 "lex.yy.c"
+#line 518 "lex.yy.c"
 
 	if ( yy_init )
 		{
@@ -566,13 +565,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 8 )
+				if ( yy_current_state >= 9 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 5 );
+		while ( yy_base[yy_current_state] != 6 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -600,20 +599,27 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 7 "program2.l"
-{vow_count++;}
+#line 6 "program3.l"
+{
+	nchars++;nlines++; 
+	}
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 8 "program2.l"
-{const_count++;}
+#line 10 "program3.l"
+{nwords++, nchars=nchars+yyleng;}
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 9 "program2.l"
+#line 11 "program3.l"
+{nchars++;}
+	YY_BREAK
+case 4:
+YY_RULE_SETUP
+#line 12 "program3.l"
 ECHO;
 	YY_BREAK
-#line 617 "lex.yy.c"
+#line 623 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -905,7 +911,7 @@ static yy_state_type yy_get_previous_state()
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 8 )
+			if ( yy_current_state >= 9 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -940,11 +946,11 @@ yy_state_type yy_current_state;
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 8 )
+		if ( yy_current_state >= 9 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 7);
+	yy_is_jam = (yy_current_state == 8);
 
 	return yy_is_jam ? 0 : yy_current_state;
 	}
@@ -1499,14 +1505,17 @@ int main()
 	return 0;
 	}
 #endif
-#line 9 "program2.l"
+#line 12 "program3.l"
 
-int yywrap(){}
-int main()
+int yywrap(void)  
 {
-	printf("Enter the string of vowels and consonents:");
-	yylex();
-	printf("Number of vowels are: %d\n", vow_count);
-	printf("Number of consonants are: %d\n", const_count);
-	return 0;
+ return 1;
+}
+int main(int argc, char*argv[]) 
+{
+	yyin=fopen(argv[1],"r");
+ yylex();
+ printf("Lines = %d\nChars=%d\nWords=%d",nlines,nchars,nwords);
+
+ return 0;
 }
